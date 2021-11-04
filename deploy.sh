ffe078e481881fd1e3315352acbf2cba07e39444
dusty <dusty@yandex-team.ru>
2021-11-04 16:48:40 +0300

    fixup! fix deploy

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,6 @@ cd build
 # если вы публикуете на пользовательский домен
 # echo 'www.example.com' > CNAME
 
-git config --global credential.helper store
-echo 'https://$($env:access_token):x-oauth-basic@github.com`n' > "$HOME\.git-credentials"
-git config --global user.email "a.shleyko@yandex.ru"
-git config --global user.name "Alexandr Shleyko"
-
 git init
 git add -A
 git commit -m 'deploy'
